786569778713189e3b280fc6eca964b05d5c1ac7
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-15 14:17:04 +0300

    fixed first line spelling error

--- markdown2html.py
+++ markdown2html.py
@@ -1,36 +1,97 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """This module takes a readme text and translates it to html code"""
 import sys
 import os
 
 
 def convert_markdown_to_html(markdown_file, output_file):
-    """ The function responiable of transforming from markdown to html code"""
-    with open(markdown_file, 'r') as md_file, open(output_file, 'w') as html_file:
-        in_list = False
+    """ The function responsiable of transforming from markdown to html code"""
+    with open(markdown_file, 'r') as md_file, open(output_file,
+                                                   'w') as html_file:
+        in_ulist = False
+        in_olist = False
+        in_paragraph = False
+
         for line in md_file:
             line = line.rstrip()
+
+            # Convert bold syntax using ** and __
+            line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+            line = re.sub(r'__(.*?)__', r'<b>\1</b>', line)
+
+            # Heading parsing
             if line.startswith('#'):
-                if in_list:
+                if in_ulist:
                     html_file.write('</ul>\n')
-                    in_list = False
+                    in_ulist = False
+                if in_olist:
+                    html_file.write('</ol>\n')
+                    in_olist = False
+                if in_paragraph:
+                    html_file.write('</p>\n')
+                    in_paragraph = False
+
                 header_level = line.count('#', 0, 6)
                 if header_level > 0 and line[header_level] == ' ':
                     content = line[header_level + 1:]
-                    html_file.write(f'<h{header_level}>{content}</h{header_level}>\n')
+                    html_file.write
+                    (f'<h{header_level}>{content}</h{header_level}>\n')
+
+            # Unordered list parsing
             elif line.startswith('- '):
-                if not in_list:
+                if in_olist:
+                    html_file.write('</ol>\n')
+                    in_olist = False
+                if in_paragraph:
+                    html_file.write('</p>\n')
+                    in_paragraph = False
+                if not in_ulist:
                     html_file.write('<ul>\n')
-                    in_list = True
+                    in_ulist = True
                 content = line[2:]
                 html_file.write(f'<li>{content}</li>\n')
-            else:
-                if in_list:
+
+            # Ordered list parsing
+            elif line[0].isdigit() and line[1] == '.' and line[2] == ' ':
+                if in_ulist:
                     html_file.write('</ul>\n')
-                    in_list = False
+                    in_ulist = False
+                if in_paragraph:
+                    html_file.write('</p>\n')
+                    in_paragraph = False
+                if not in_olist:
+                    html_file.write('<ol>\n')
+                    in_olist = True
+                content = line[3:]
+                html_file.write(f'<li>{content}</li>\n')
+
+            # Paragraph parsing
+            elif line.strip():
+                if in_ulist:
+                    html_file.write('</ul>\n')
+                    in_ulist = False
+                if in_olist:
+                    html_file.write('</ol>\n')
+                    in_olist = False
+                if not in_paragraph:
+                    html_file.write('<p>\n')
+                    in_paragraph = True
                 html_file.write(line + '\n')
-        if in_list:
+
+            # Empty line handling
+            else:
+                if in_paragraph:
+                    html_file.write('</p>\n')
+                    in_paragraph = False
+                html_file.write(line + '\n')
+
+        if in_ulist:
             html_file.write('</ul>\n')
+        if in_olist:
+            html_file.write('</ol>\n')
+        if in_paragraph:
+            html_file.write('</p>\n')
+
 
 def main():
     """main function that checks for argument length"""
