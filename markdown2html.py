1bf14b0bdf376cd6bc21105666dbfaf83cfc38c3
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-15 13:56:04 +0300

    updated by adding a function that converts from markdown to html code

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,27 @@
-#!/usr/bin/python3
-"""This module takes 2 files as inputs to insert the readme inside the html"""
+#!/usr/bin/env python3
+"""This module takes a readme text and translates it to html code"""
 import sys
 import os
 
 
+def convert_markdown_to_html(markdown_file, output_file):
+    """ The function responiable of transforming from markdown to html code"""
+    with open(markdown_file, 'r') as md_file, open(output_file,
+                                                   'w') as html_file:
+        for line in md_file:
+            line = line.rstrip()
+            if line.startswith('#'):
+                header_level = line.count('#', 0, 6)
+                if header_level > 0 and line[header_level] == ' ':
+                    content = line[header_level + 1:]
+                    html_file.
+                    write(f'<h{header_level}>{content}</h{header_level}>\n')
+            else:
+                html_file.write(line + '\n')
+
+
 def main():
-    """starting point"""
+    """main function that checks for argument length"""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -17,6 +33,8 @@ def main():
     if not os.path.exists(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
+
+    convert_markdown_to_html(markdown_file, output_file)
 
     sys.exit(0)
 
